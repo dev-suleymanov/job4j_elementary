d210d6505d3418c241d2c75391db4a090d8f101d
dev-suleymanov <dev.suleymanov@gmail.com>
2022-10-20 14:48:55 +0300

    9.3. Преобразование примитивных типов [#383106]

---
+++ src/main/java/ru/job4j/converter/Types.java
@@ -0,0 +1,27 @@
+package ru.job4j.converter;
+
+public class Types {
+    public static void main(String[] args) {
+        long l = 129;
+        byte b = (byte) l;
+        System.out.println(b);
+
+        float f = 12358.7f;
+        int i = 45981;
+        short rsl = (short) (f + i);
+        System.out.println(rsl);
+
+        char c = 45000;
+        float x = c;
+        System.out.println(x);
+
+        double d = 121.19;
+        byte y = (byte) d;
+        System.out.println(y);
+
+        short s = 1500;
+        char j = (char) s;
+        double v = c;
+        System.out.println(v);
+    }
+}
