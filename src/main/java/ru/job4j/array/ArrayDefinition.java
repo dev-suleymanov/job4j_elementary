913105c787f111985e71aafd1867592cf1e71a26
dev-suleymanov <dev.suleymanov@gmail.com>
2022-11-01 20:44:12 +0300

    6.0.2. Размер массива [#333582]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,8 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        System.out.println("ages size: " + ages.length);
+        System.out.println("surnames size: " + surnames.length);
+        System.out.println("prices size: " + prices.length);
     }
 }
