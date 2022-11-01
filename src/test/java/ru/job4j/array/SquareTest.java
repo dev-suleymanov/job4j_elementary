04d6f3e97cf60cab94368886e8629ce7169c6f44
dev-suleymanov <dev.suleymanov@gmail.com>
2022-11-01 21:35:56 +0300

    6.0.7. Массивы в тестах. [#226789]

--- src/test/java/ru/job4j/array/SquareTest.java
+++ src/test/java/ru/job4j/array/SquareTest.java
@@ -5,28 +5,19 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SquareTest {
-
     @Test
-    public void when2() {
-        int bound = 2;
-        int[] arrayIn = {0, 1};
-        int[] arrayOut = Square.calculate(bound);
-        assertThat(arrayIn).isEqualTo(arrayOut);
+    public void whenBound3Then014() {
+        int bound = 3;
+        int[] result = Square.calculate(bound);
+        int[] expected = new int[]{0, 1, 4};
+        assertThat(result).containsExactly(expected);
     }
 
     @Test
-    public void when4() {
-        int bound = 4;
-        int[] arrayIn = {0, 1, 4, 9};
-        int[] arrayOut = Square.calculate(bound);
-        assertThat(arrayIn).isEqualTo(arrayOut);
-    }
-
-    @Test
-    public void when9() {
-        int bound = 9;
-        int[] arrayIn = {0, 1, 4, 9, 16, 25, 36, 49, 64};
-        int[] arrayOut = Square.calculate(bound);
-        assertThat(arrayIn).isEqualTo(arrayOut);
+    public void whenBound5Then014916() {
+        int bound = 5;
+        int[] result = Square.calculate(bound);
+        int[] expected = new int[]{0, 1, 4, 9, 16};
+        assertThat(result).containsExactly(expected);
     }
 }
