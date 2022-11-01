8d7e4a78988f6bedff78c839482834c10dd8f183
dev-suleymanov <dev.suleymanov@gmail.com>
2022-11-01 21:14:47 +0300

    6.0.5. Массивы и цикл for-each [#333581]

--- src/main/java/ru/job4j/condition/ThreeMax.java
+++ src/main/java/ru/job4j/condition/ThreeMax.java
@@ -2,19 +2,20 @@ package ru.job4j.condition;
 
 public class ThreeMax {
     public static int max(int first, int second, int third) {
-        int result = first;
-        if (second > first && second > third) {
-            result = second;
-        }
-        if (third > result) {
-            result = third;
+        int result = 0;
+        if (first > second) {
+            if (first > third) {
+                result = first;
+            } else {
+                result = third;
+            }
+        } else {
+            if (second > third) {
+                result = second;
+            } else {
+                result = third;
+            }
         }
         return result;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(max(0, 11, 15));
-        System.out.println(max(12, 25, 25));
-        System.out.println(max(-25, 14, 32));
     }
 }
