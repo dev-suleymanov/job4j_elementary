31fc0790b049177d7d6d696c7b97850b0fa40541
dev-suleymanov <dev.suleymanov@gmail.com>
2022-11-02 12:04:43 +0300

    4.5. Отладка программы в IDEA [#159464 #356297]

--- src/main/java/ru/job4j/condition/ThreeMax.java
+++ src/main/java/ru/job4j/condition/ThreeMax.java
@@ -2,19 +2,12 @@ package ru.job4j.condition;
 
 public class ThreeMax {
     public static int max(int first, int second, int third) {
-        int result = 0;
-        if (first > second) {
-            if (first > third) {
-                result = first;
-            } else {
-                result = third;
-            }
-        } else {
-            if (second > third) {
-                result = second;
-            } else {
-                result = third;
-            }
+        int result = first;
+        if (second >= first && second >= third) {
+            result = second;
+        }
+        if (third >= first && third >= second) {
+            result = third;
         }
         return result;
     }
