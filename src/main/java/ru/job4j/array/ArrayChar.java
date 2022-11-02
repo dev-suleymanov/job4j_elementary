6bed2ecb24c51315d66e1a53ef7e4f4b4b3c63e3
dev-suleymanov <dev.suleymanov@gmail.com>
2022-11-02 23:41:11 +0300

    6.4.0. Слово начинается с ... [#41585 #357395]

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -4,8 +4,11 @@ public class ArrayChar {
 
     public static boolean startsWith(char[] word, char[] pref) {
         boolean result = true;
-        for (int i = 0; i < pref.length; i++) {
-            result = word[i] == pref[i];
+        for (int i = 0; i < word.length; i++) {
+            if (i == pref.length - 1) {
+                result = word[i] == pref[i];
+                break;
+            }
         }
         return result;
     }
