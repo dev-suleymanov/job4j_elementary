4f15dabd6f155e221e5e969b2f2dc8f6c13b6c36
dev-suleymanov <dev.suleymanov@gmail.com>
2022-11-05 15:19:46 +0300

    6.6.4. Двухмерный массив. Таблица умножения. [#33491 #357553]

--- src/main/java/ru/job4j/array/Matrix.java
+++ src/main/java/ru/job4j/array/Matrix.java
@@ -3,9 +3,9 @@ package ru.job4j.array;
 public class Matrix {
     public static int[][] multiple(int size) {
         int[][] array = new int[size][size];
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                array[i][j] = (i + 1) * (j + 1);
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                array[i - 1][j - 1] = i * j;
             }
         }
         return array;
--- src/main/java/ru/job4j/array/SortSelected.java
+++ src/main/java/ru/job4j/array/SortSelected.java
@@ -2,11 +2,9 @@ package ru.job4j.array;
 
 public class SortSelected {
     public static int[] sort(int[] data) {
-        int min = 0;
-        int index = 0;
         for (int i = 0; i < data.length; i++) {
-            min = MinDiapason.findMin(data, i, data.length - 1);
-            index = FindLoop.indexInRange(data, min, i, data.length - 1);
+            int min = MinDiapason.findMin(data, i, data.length - 1);
+            int index = FindLoop.indexInRange(data, min, i, data.length - 1);
             SwitchArray.swap(data, index, i);
         }
         return data;
