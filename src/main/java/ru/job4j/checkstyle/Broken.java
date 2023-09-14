f95005bcbeb4c468ba5c5ebfb3d246380b0e4067
dev-suleymanov <dev.suleymanov@gmail.com>
2023-09-14 21:55:36 +0300

    6. Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,27 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+    public static final String NEW_VALUE = "";
+
+    public String name;
+
+    public String surname;
+
+    private int size = 10;
+
+    public Broken() {
+    }
+
+    public void echo() {
+    }
+
+    public void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    public void method(int a, int b, int c, int d, int e, int f, int g) {
+
+    }
+}
