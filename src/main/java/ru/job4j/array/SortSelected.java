20c534d6cbdec1630985fba3ba5efbf8efd009a2
dev-suleymanov <dev.suleymanov@gmail.com>
2024-10-14 16:58:47 +0300

    GPT. Нейросети спешат на помощь. [#505156]

--- src/main/java/ru/job4j/array/SortSelected.java
+++ src/main/java/ru/job4j/array/SortSelected.java
@@ -1,12 +1,19 @@
 package ru.job4j.array;
 
+import java.util.Arrays;
+
 public class SortSelected {
     public static int[] sort(int[] data) {
         for (int i = 0; i < data.length; i++) {
             int min = MinDiapason.findMin(data, i, data.length - 1);
             int index = FindLoop.indexInRange(data, min, i, data.length - 1);
-            SwitchArray.swap(data, index, i);
+            SwitchArray.swap(data, i, index);
         }
         return data;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {8, 1, 2, -1, 0, -15, 22, 12, 14};
+        System.out.println(Arrays.toString(sort(array)));
     }
 }
